12dd08e66e20e247b189370ae6f15779b448bdfd
truscomp4-sys <truscomp4@gmail.com>
2026-03-07 14:04:15 +0530

    Delete dist directory

--- dist/routes/admin.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_middleware_1 = require("../middleware/auth.middleware");
-const dashboard_controller_1 = require("../controllers/dashboard.controller");
-const router = (0, express_1.Router)();
-// Integrated Dashboard Stats
-router.get('/dashboard/stats', auth_middleware_1.verifyJWT, auth_middleware_1.isAdmin, dashboard_controller_1.getDashboardStats);
-exports.default = router;
